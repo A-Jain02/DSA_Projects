eb9a987b22b02983e6a8436273636407e282bab0
A-Jain02 <152732053+A-Jain02@users.noreply.github.com>
2025-07-10 16:01:55 +0530

    Rename judge.h to Multiplayer Coding Game/judge.h

---
+++ Multiplayer Coding Game/judge.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <string>
+
+int judgeAnswer(const std::string& problem, const std::string& answer);
--- judge.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <string>
-
-int judgeAnswer(const std::string& problem, const std::string& answer);
